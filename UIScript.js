a7e4791ee6778f5506bff0852040ac4154e217fa
Madison Reynolds <75910495+MossyStone@users.noreply.github.com>
2022-10-29 17:12:48 -0500

    date semi working

--- UIScript.js
+++ UIScript.js
@@ -12,8 +12,34 @@ function decideToggle(){
 
 function openNav(){
     document.getElementById("ReportSideBar").style.display = "block"
+    
+    let date = grabDate()
+    document.getElementById("date").innerHTML = date
 }
 function closeNav(){
     document.getElementById("ReportSideBar").style.display = "none"
 }
 //End of Nav Bar Codes
+
+//Start of Date Time Entry
+function grabDate(){
+    const holdingDate = new Date()
+    
+    var day = holdingDate.getDay()
+    var month = holdingDate.getMonth()
+    var year = holdingDate.getFullYear()
+    var hour = holdingDate.getHours()
+    var min = holdingDate.getMinutes()
+    var sec = holdingDate.getSeconds()
+
+    var combinedDate = "" + year + "-" + month.padStart(2, '0') + "-" + day.padStart(2, '0')
+    var combinedTime = "" + hour.padStart(2, '0') + ":" + min.padStart(2, '0') + ":" + sec.padStart(2, '0')
+
+    var combinedAll = combinedDate + " " + combinedTime
+    
+    //document.getElementById("TESTER").innerHTML = combinedAll
+
+    return combinedAll
+ }
+
+//End of Date/Time Entry
