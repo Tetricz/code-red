e9dea475e25434f24a7b2e31b54176d2583b1143
Madison Reynolds <75910495+MossyStone@users.noreply.github.com>
2022-10-29 17:24:24 -0500

    Date finished

--- UIScript.js
+++ UIScript.js
@@ -25,19 +25,19 @@ function closeNav(){
 function grabDate(){
     const holdingDate = new Date()
     
-    var day = holdingDate.getDay()
-    var month = holdingDate.getMonth()
-    var year = holdingDate.getFullYear()
-    var hour = holdingDate.getHours()
-    var min = holdingDate.getMinutes()
-    var sec = holdingDate.getSeconds()
+    var day =  "" + holdingDate.getDay()
+    var month = "" + holdingDate.getMonth()
+    var year = "" + holdingDate.getFullYear()
+    var hour = "" + holdingDate.getHours()
+    var min = "" + holdingDate.getMinutes()
+    var sec = "" + holdingDate.getSeconds()
 
     var combinedDate = "" + year + "-" + month.padStart(2, '0') + "-" + day.padStart(2, '0')
     var combinedTime = "" + hour.padStart(2, '0') + ":" + min.padStart(2, '0') + ":" + sec.padStart(2, '0')
 
     var combinedAll = combinedDate + " " + combinedTime
     
-    //document.getElementById("TESTER").innerHTML = combinedAll
+    document.getElementById("TESTER").innerHTML = combinedAll
 
     return combinedAll
  }
