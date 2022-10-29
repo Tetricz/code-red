1b36d5ea28630097918616ed0100a63dabb1ae9e
Madison Reynolds <75910495+MossyStone@users.noreply.github.com>
2022-10-29 15:01:18 -0500

    Ui Scripts for Web Page

---
+++ UIScript.js
@@ -0,0 +1,10 @@
+//Start of the nav bar codes
+//Insert Nav ID
+function openNavBar(){
+    document.getElementById("").style.display = "block";
+}
+
+function closeNavBar(){
+    document.getElementById("").style.display = "None"
+}
+//End of Nav Bar Codes
