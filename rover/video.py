b3eb43d4cb6d02238a8b8dde75df1c6aa49e0d24
Tetricz <DavidLeeDaniels@protonmail.com>
2022-10-30 03:32:53 -0500

    finalize video.py

--- rover/video.py
+++ rover/video.py
@@ -8,7 +8,7 @@ import random
 
 
 class video(data):
-    def __init__(self,_lat,_long,_battery_level,_vid_name,_size):
+    def __init__(self,_lat,_long,_battery_level):
         super().__init__(_lat,_long,_battery_level)
         self.vid_name = str(self.datetime).replace(" ", "_") + ".mkv";
         self.size = 60 * random.randrange(40, 60)
@@ -31,3 +31,6 @@ class video(data):
             
 if __name__ == "__main__":
     print("Hi :D")
+    v = video(1,1,1)
+    print(v.vid_name)
+    print(v.size)
