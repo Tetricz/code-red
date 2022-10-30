29681cd3a9c72fe86fd9ed8d5f4fe597d9882949
Tetricz <DavidLeeDaniels@protonmail.com>
2022-10-30 00:14:49 -0500

    update

--- rover/main.py
+++ rover/main.py
@@ -1,6 +1,10 @@
 import database_coms as dbcoms
+import rover
 
 if __name__ == "__main__":
     db = dbcoms.DatabaseComs()
-    db.seedDatabase()
-    
+    r = rover(None, None, None, None)
+    r.get_velocity(50.0,50.05,20.0,70.0)
+    r.check_battery()
+
+    db.close()
--- rover/rover.py
+++ rover/rover.py
@@ -1,10 +1,18 @@
 class rover:
     def __init__(self,_id,_battery,_total_memory,_used_memory): #constructor
-        self.id = _id;
-        self.battery = _battery;
-        self.total_memory = _total_memory;
-        self.used_memory = _used_memory;
-    
+        self.id = _id
+        self.battery = _battery
+        self.total_memory = _total_memory
+        self.used_memory = _used_memory
+
+    def get_velocity(self, x1, y1, x2, y2):
+        x = x1 - x2
+        y = y1 - y2
+
+        direction = (y/x)
+        
+        return direction
+
     def check_battery():
         pass;
     def upload():
@@ -12,3 +20,7 @@ class rover:
     def movement():
         pass;
 
+
+if __name__ == "__main__":
+    r = rover(None, None, None, None)
+    r.get_velocity(50.0,50.05,20.0,70.0)
