e7a7ecc7f6fd4e25889fc688ef5b75509c0440f3
Madison Reynolds <75910495+MossyStone@users.noreply.github.com>
2022-10-30 03:29:48 -0500

    Created full graph

--- maps.js
+++ maps.js
@@ -3,5 +3,35 @@ function generateMap(){
     const canvas = document.getElementById("canvas")
     const ctx = canvas.getContext("2d")
 
+    for(r = 0; r < 50; r++){
+        ctx.moveTo(r*20, 0);
+        ctx.lineTo(r*20, 500);
+        ctx.stroke();
+    }
     
+    for(c = 0; c < 50; c++){
+        ctx.moveTo(0, c*20);
+        ctx.lineTo(500, c*20);
+        ctx.stroke();
+    }
+
+    canvas.style.display = "none"
+    document.getElementById("warning").style.display="block"
+}
+
+function makeIcons(rdBttn){
+   const canvas = document.getElementById("canvas")
+   canvas.style.display = "block"
+   document.getElementById("warning").style.display="none"
+
+    var radio = rdBttn.id
+
+    if(radio === "roverSelection1"){
+        document.getElementById("test").innerHTML = "yes"
+    } else if(radio === "roverSelection2"){
+        document.getElementById("test").innerHTML = "maybe"
+    } 
+    else{
+        document.getElementById("test").innerHTML = "no"
+    }
 }
